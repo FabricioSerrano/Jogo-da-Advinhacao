99347cbe79156c1cbc5547fd5d598177260ac061
Fabricio Serrano <serranobraga@gmail.com>
2021-11-26 13:59:46 -0300

    Update nas chances
    Reformulado o loop das chances do usuário  e menores ajustes nas dicas

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,7 +4,7 @@ import random
 
 #utils
 numero_secreto = random.randint(0,100)
-vidas = 3
+chances = 0
 
 #head
 
@@ -18,39 +18,52 @@ print('Foi gerado um número aleatório entre 0 e 100 e cabe a você acertar')
 #wait
 time.sleep(2)
 
+#chances
+print('Você tem 4 chances de acertar')
+
 #dica
 
 if numero_secreto < 10 :
     print('Dica: O número gerado é menor do que 10')
+
 elif numero_secreto >= 10 and numero_secreto < 20 :
     print('Dica: O número gerado é menor do que 20')
+
 elif numero_secreto >= 20 and numero_secreto < 30:
     print('Dica: O número gerado é menor do que 30')
+
 elif numero_secreto >= 30 and numero_secreto < 40:
     print('Dica: O número gerado é menor do que 40')
+
 elif numero_secreto >= 40 and numero_secreto < 50:
     print('Dica: O número gerado é menor do que 50')
+
 elif numero_secreto >= 50 and numero_secreto < 60:
-    print('Dica: O número gerado é menor do que 60')
+    print('Dica: O número gerado é maior do que 50')
+
 elif numero_secreto >= 60 and numero_secreto < 70:
-    print('Dica: O número gerado é menor do que 70')
+    print('Dica: O número gerado é maior do que 60')
+
 elif numero_secreto >= 70 and numero_secreto < 80:
-    print('Dica: O número gerado é menor do que 80')
+    print('Dica: O número gerado é maior do que 70')
+
 elif numero_secreto >= 80 and numero_secreto < 90:
-    print('Dica: O número gerado é menor do que 90')
+    print('Dica: O número gerado é maior do que 80')
+
 elif numero_secreto >= 90 :
-    print('Dica: O número gerado esta entre 90 e 100')
+    print('Dica: O número gerado é maior do que 90')
 
 time.sleep(1)
 
 
 #contagem de vidas  
-while vidas > 0:
+for chances in range(1,5):
 
-    #vidas
+    #chances
+    print('chance nº {}'.format(chances))
 
-    print('Você tem {} vidas'.format(vidas))
-    print(' ')
+
+    
 
     time.sleep(1)
 
@@ -70,26 +83,27 @@ while vidas > 0:
     if acerto:
         print('VOCÊ ACERTOU... \n O número secreto era {}'.format(numero_secreto))
         print(' ')
-        vidas = 0
+        
 
         print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
         print('           CONGRATULATIONS          ')
         print('____________________________________')
         print('O número secreto era {}'.format(numero_secreto))
         print(' ')
+        break
 
     else:
         if Mais:
             print('HUUUUL POR CIMA DA TRAVE... \n O número secreto é menor do que {}'.format(chute))
             print(' ')
-            vidas -=1
+            chances +=1
 
         elif Menos:
             print('ERROOOOOOU... \n O número secreto é maior do que {}'.format(chute))
             print(' ')
-            vidas -=1
+            chances +=1
 
-        if vidas == 0 :
+        if chances == 5 :
             print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
             print('              GAME OVER             ')
             print('____________________________________')
